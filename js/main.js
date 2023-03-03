69508e2d14ad1f8201b6b4651702e7ca543945fa
a-wotton <a_wotton@fanshaweonline.ca>
2023-03-03 10:03:36 -0500

    setup js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 // drum kit: press a key to play the sound. use JavaScript to retrieve the data-key attribute and then use that as a selector to find the matching audio file and play it!
-
-(() => {
-    console.log('music player script file');
     
-})();
+console.log('music player script file');
+
+window.addEventListener('keyup', findMatchingAudio);
+
+function findMatchingAudio(event) {
+    debugger;
+}
+
+
+    
